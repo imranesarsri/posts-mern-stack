9af07619a5e78e426d0b8354efca6de3ef7663a8
imrane sarsri <imransarsi.ii@gmail.com>
2024-10-15 22:46:43 +0100

    modify button login

--- frontEnd/src/components/FormControls/ButtonsForm.jsx
+++ frontEnd/src/components/FormControls/ButtonsForm.jsx
@@ -19,7 +19,7 @@ export function LoginButton() {
 
     return (
         <Link
-            className="block text-center capitalize w-full text-Dark-text bg-Light-primary dark:bg-Dark-primary dark:hover:bg-Dark-secondary hover:bg-Light-secondary focus:outline-none focus:ring font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 transition delay-100 duration-150 ease-in-out"
+            className="block text-center capitalize w-full text-Dark-text bg-Light-primary dark:bg-Dark-primary dark:hover:bg-Dark-secondary hover:bg-Light-secondary focus:outline-none focus:ring font-medium rounded-full text-sm px-5 py-2.5 transition delay-100 duration-150 ease-in-out"
             to="/login"
         >
             {translate('buttons:loginButton')}
